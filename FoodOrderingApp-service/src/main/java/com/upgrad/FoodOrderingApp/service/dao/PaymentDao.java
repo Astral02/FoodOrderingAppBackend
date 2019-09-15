b074ac9ded7d5dd6930202a6bf870b82b09938cf
Vikas Garg <vikas04522@gmail.com>
2019-09-15 14:04:01 -0400

    Order 2 (#23)
    * Removed old entity class and updated new updated classes
    
    * Added Dao for Order and Order Item
    
    * Updated the customerDao, orderDao, ItemService and added order service
    
    * Added the controller for the orders

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/PaymentDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/PaymentDao.java
@@ -4,6 +4,7 @@ import com.upgrad.FoodOrderingApp.service.entity.PaymentEntity;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
@@ -16,5 +17,12 @@ public class PaymentDao {
 
         List<PaymentEntity> payments =  entityManager.createNamedQuery("getPayment",PaymentEntity.class).getResultList();
         return payments;
+    }
+    public PaymentEntity getPaymentByUUID(String uuid) {
+        try {
+            return entityManager.createNamedQuery("paymentByUUID", PaymentEntity.class).setParameter("uuid", uuid).getSingleResult();
+        } catch (NoResultException ex) {
+            return null;
+        }
     }
 }
